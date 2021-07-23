7475385b54a2e3192f6d0d6ddfadec58bcd40b8c
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-07-23 01:58:10 -0600

    Dashboard module added.

--- ngbusiness-frontend/src/app/app-routing.module.ts
+++ ngbusiness-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: "",
+    loadChildren: () => import("./modules/dashboard/dashboard.module").then(module => module.DashboardModule)
+  },
+  {
     path: "clients",
     loadChildren: () => import("./modules/clients/clients.module").then(module => module.ClientsModule)
   },
   {
     path: "products",
     loadChildren: () => import("./modules/products/products.module").then(module => module.ProductsModule)
+  },
+  {
+    path: "**",
+    redirectTo: "/"
   }
 ];
 
